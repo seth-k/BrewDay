040a10ee247d9a5cec83af21a21a9bd431ee28cd
Seth Kroger <seth.kroger@gmail.com>
2016-03-09 21:16:04 -0800

    Create number picker with range and number format settable in XML

--- app/src/main/java/seth_k/app/brewday/ui/RangedNumberPicker.java
+++ app/src/main/java/seth_k/app/brewday/ui/RangedNumberPicker.java
@@ -2,10 +2,21 @@ package seth_k.app.brewday.ui;
 
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.widget.NumberPicker;
 
-public class RangedNumberPicker extends NumberPicker {
+import seth_k.app.brewday.R;
+
+public class RangedNumberPicker extends NumberPicker implements NumberPicker.Formatter {
+
+    public static final String FORMAT = "%.1f";
+    public static final String NAMESPACE = "http://schemas.android.com/apk/res-auto";
+    private double mRangeStart;
+    private double mRangeEnd;
+    private double mRangeStep;
+    private String mFormatString;
+
 
     public RangedNumberPicker(Context context) {
         super(context);
@@ -13,6 +24,59 @@ public class RangedNumberPicker extends NumberPicker {
 
     public RangedNumberPicker(Context context, AttributeSet attrs) {
         super(context, attrs);
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.RangedNumberPicker,0,0);
+
+        try {
+            mRangeStart = a.getFloat(R.styleable.RangedNumberPicker_rangeStart, 0.0f);
+            mRangeEnd = a.getFloat(R.styleable.RangedNumberPicker_rangeEnd, 10.0f);
+            mRangeStep = a.getFloat(R.styleable.RangedNumberPicker_rangeStep, 1.0f);
+            mFormatString = a.getString(R.styleable.RangedNumberPicker_formatString);
+            if(mFormatString == null) {
+                mFormatString = FORMAT;
+            }
+        } finally {
+            a.recycle();
+        }
+
+        int rangeLength = ((Double)( (mRangeEnd - mRangeStart) / mRangeStep)).intValue() + 1;
+        setMinValue(0);
+        setMaxValue(rangeLength-1);
     }
 
+    public void setRangeStart(double rangeStart) {
+        mRangeStart = rangeStart;
+    }
+
+    public void setRangeEnd(double rangeEnd) {
+        mRangeEnd = rangeEnd;
+    }
+
+    public double getRangeStart() {
+        return mRangeStart;
+    }
+
+    public double getRangeEnd() {
+        return mRangeEnd;
+    }
+
+    public double getRangeStep() {
+        return mRangeStep;
+    }
+
+    public void setRangeStep(double rangeStep) {
+        mRangeStep = rangeStep;
+    }
+
+    public String getFormatString() {
+        return mFormatString;
+    }
+
+    public void setFormatString(String formatString) {
+        mFormatString = formatString;
+    }
+
+    @Override
+    public String format(int value) {
+        return String.format(mFormatString, mRangeStart + mRangeStep * ((Integer)value).doubleValue());
+    }
 }
--- app/src/test/java/seth_k/app/brewday/ui/RangedNumberPickerTest.java
+++ app/src/test/java/seth_k/app/brewday/ui/RangedNumberPickerTest.java
@@ -13,23 +13,75 @@ import org.robolectric.annotation.Config;
 import seth_k.app.brewday.BuildConfig;
 import seth_k.app.brewday.R;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(RobolectricGradleTestRunner.class)
 @Config(constants = BuildConfig.class, sdk=21)
 public class RangedNumberPickerTest {
+    public static final double DELTA = 0.0000001;
     private RangedNumberPicker mPicker;
 
+    /**
+     * Sets up a RangedNumberPicker from a test XML file.  The file specifies a range of [10, 20]
+     * with a step of 2.
+     * @throws Exception
+     */
     @Before
     public void setUp() throws Exception {
-
+        Activity activity = Robolectric.setupActivity(Activity.class);
+        mPicker = (RangedNumberPicker) LayoutInflater.from(activity)
+                .inflate(R.layout.test_ranged_number_picker, null);
     }
 
     @Test
     public void constructsNumberPickerFromXml() throws Exception {
-        Activity activity = Robolectric.setupActivity(Activity.class);
-        mPicker = (RangedNumberPicker) LayoutInflater.from(activity)
-                .inflate(R.layout.test_ranged_number_picker, null);
+
         assertNotNull(mPicker);
+    }
+
+    @Test
+    public void setsRangeStart() throws Exception {
+        mPicker.setRangeStart(2.0);
+
+        assertEquals(2.0, mPicker.getRangeStart(), DELTA);
+    }
+
+    @Test
+    public void setsRangeStartFromXml() throws Exception {
+        // XML will set range to (10, 20, 2)
+        assertEquals(10.0, mPicker.getRangeStart(), DELTA);
+        assertEquals(20.0, mPicker.getRangeEnd(), DELTA);
+        assertEquals(2.0, mPicker.getRangeStep(), DELTA);
+    }
+
+    @Test
+    public void rangeIntervalHasSixSteps() throws Exception {
+        assertEquals(6, mPicker.getMaxValue() - mPicker.getMinValue() + 1);
+    }
+
+    @Test
+    public void setsStartLabelToTenPointOh() throws Exception {
+        assertEquals("10.0", mPicker.format(0));
+    }
+
+    @Test
+    public void setsSecondLabelToTwelvePointOh() throws Exception {
+        assertEquals("12.0", mPicker.format(1));
+    }
+
+    @Test
+    public void changingFormatStringUsesNewFormat() throws Exception {
+        mPicker.setFormatString("%2.0f");
+        assertEquals("10", mPicker.format(0));
+    }
+
+    @Test
+    public void formatStringCanBeSetInXml() throws Exception {
+        Activity activity = Robolectric.setupActivity(Activity.class);
+        RangedNumberPicker picker = (RangedNumberPicker) LayoutInflater.from(activity)
+                .inflate(R.layout.test_ranged_number_picker_formated, null);
+
+
     }
 }
